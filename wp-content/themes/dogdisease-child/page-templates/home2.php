2053d543287225a8a3fce2bdfa202cac9c9d6b6c
Drew <akeller@eight-eleven.com>
2015-02-12 12:12:47 -0500

    updated docs

--- wp-content/themes/dogdisease-child/page-templates/home2.php
+++ wp-content/themes/dogdisease-child/page-templates/home2.php
@@ -25,11 +25,11 @@ get_header(); ?>
 								<h3>eBook Chapters</h3>
 								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dogPref.docx" class="icon-doc">Preface</a>
 								<!-- <a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/editorial-comments.docx" class="icon-doc">Editorial Commentary</a> -->
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog-idea-analysis.docx" class="icon-doc">Idea Analysis as a Research Tool</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog-cooperative-research.docx" class="icon-doc">Advantages of Cooperative Research</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog-creative-and-critical-thinking.docx" class="icon-doc">Creative Critical Thinking</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog-analysis-of-a-document.docx" class="icon-doc">Enhancing Peer Review</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog-algorithms.docx" class="icon-doc">Algorithms and Transparent Critical Thinking</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog1.docx" class="icon-doc">Idea Analysis as a Research Tool</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2.docx" class="icon-doc">Advantages of Cooperative Research</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog3.docx" class="icon-doc">Creative Critical Thinking</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog4.docx" class="icon-doc">Enhancing Peer Review</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog5.docx" class="icon-doc">Algorithms and Transparent Critical Thinking</a>
 							</div>
 							
 							<div class="col2">
@@ -39,8 +39,13 @@ get_header(); ?>
 								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog1990.xlsx" class="icon-xls">1990-1994</a>
 								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog1995.xlsx" class="icon-xls">1995-1999</a>
 								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2000.xlsx" class="icon-xls">2000-2004</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2005.xlsx" class="icon-xls">2005-2009</a>
-								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2010.xlsx" class="icon-xls">2010-2013</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2005A.xlsx" class="icon-xls">2005</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2006.xlsx" class="icon-xls">2006</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2007.xlsx" class="icon-xls">2007</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2008.xlsx" class="icon-xls">2008</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2009.xlsx" class="icon-xls">2009</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2010-11.xlsx" class="icon-xls">2010-2011</a>
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/docs/dog2012-13.xlsx" class="icon-xls">2012-2013</a>
 							</div>
 					
 						</div> <!-- /resources -->
