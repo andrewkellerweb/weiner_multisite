541edea82ed963bf01b7900b87aae3c0dc141501
Andrew Keller <andrewkellerweb@gmail.com>
2017-10-22 12:53:54 -0400

    health-literacy - site

---
+++ wp-content/themes/health-literacy/page-home.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ Template Name: Homepage
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="content">
+		<div id="inner-content" class="wrap cf">
+
+				<main id="main" class="m-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+					<div class="wrapper-post">
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+							<?php
+								the_content();
+
+								wp_link_pages( array(
+									'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+									'after'       => '</div>',
+									'link_before' => '<span>',
+									'link_after'  => '</span>',
+								) );
+							?>
+
+					</article>
+
+					<?php endwhile; ?>
+					<?php endif; ?>
+
+					</div>
+
+				</main>
+
+			</div>
+		</div>
+
+
+<?php get_footer(); ?>
